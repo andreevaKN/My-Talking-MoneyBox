9cbe28fba6ff0b3ef5f395eb75007b5098af8b7c
KZSlayer <nikitaklimus2002@gmail.com>
2024-03-14 00:31:22 +0300

    поднастроил проект

---
+++ Assets/Scripts/ProgrammManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ProgrammManager : MonoBehaviour
+{
+    [SerializeField] private GameObject PlaneMarkerPrefab;
+
+    private ARRaycastManager ARRaycastManagerScript;
+
+    public GameObject ObjectToSpawn;
+    void Start()
+    {
+        ARRaycastManagerScript = FindObjectOfType<ARRaycastManager>();
+
+        PlaneMarkerPrefab.SetActive(false);
+    }
+
+    void Update()
+    {
+        ShowMarker();
+    }
+
+    void ShowMarker()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        ARRaycastManagerScript.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if (hits.Count > 0)
+        {
+            PlaneMarkerPrefab.transform.position = hits[0].pose.position;
+            PlaneMarkerPrefab.SetActive(true);
+        }
+
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Instantiate(ObjectToSpawn, hits[0].pose.position, ObjectToSpawn.transform.rotation);
+        }
+    }
+}
